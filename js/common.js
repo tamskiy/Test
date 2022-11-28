c4a446a570d0c18d782e22c7d5ef6b7f23ea23db
tamskiy <114107440+tamskiy@users.noreply.github.com>
2022-11-28 22:09:18 +0300

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -1,3 +1,5 @@
-let inputs = document.querySelectorAll('input[type="tel"]');
-let im = new Inputmask('+7 (777) 777-77-77');
-im.mask(inputs);
+jQuery(function($){
+
+    $("input[type='phone']").mask("(999) 999-9999");
+
+ });
